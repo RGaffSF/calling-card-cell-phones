3a8ce69e328fcd680761e9eec03740badd5cbc4b
Randall <randall.gaffner.t1w9@statefarm.com>
2021-07-28 14:00:14 -0500

    updating

--- CellPhone.js
+++ CellPhone.js
@@ -5,7 +5,7 @@ class CellPhone {
     constructor (callingCard) { //we want to track an instance of our calling card
         this.callingCard = callingCard;
         //this.phone = "";
-        this.history = []; //our call history log
+        this.history = ""; //our call history log
         this.activeCall = false;
         this.totalticks = 0;
     }
@@ -20,14 +20,24 @@ if (this.activeCall === true) {
 // Start a call
    call(phoneNumber) {  //phone number to call
     //set active call to true
-    this.activeCall = true;
-    this.history.push(phoneNumber);
+    if (this.activeCall === true) {
+        return "Call needs to be ended";
+    } else {
+      this.activeCall = true;
+      if (this.history.length > 0) {
+          this.history += ", "
+      }
+    this.history += phoneNumber;
+    };
    }; 
 
 // Let the minutes tick by.
  tick() {
-     this.callingCard.minutes - 1;
+     this.callingCard.minutes -= 1;
      this.totalticks += 1;
+     if (this.callingCard.minutes === 0 ) {
+        this.endCall()
+     }
  };
   
 // end a call
@@ -35,16 +45,29 @@ if (this.activeCall === true) {
     //set active call to false
     this.activeCall = false;
     //push minutes to a history array to create a pseudo log
-    this.history.push(this.totalticks);
+    if (this.callingCard.minutes === 0){
+        if (this.totalticks === 1){
+            this.history += " (cut of at " + this.totalticks + " minute)";    
+        } else {
+            this.history += " (cut of at " + this.totalticks + " minutes)";
+        }
+    } else {
+        if (this.totalticks === 1){
+            this.history += " (" + this.totalticks + " minute)";
+        } else {
+            this.history += " (" + this.totalticks + " minutes)";
+        }
+    }
     this.totalticks = 0;
    }; 
- }
- 
-// See the call history as a string.
-    //return the start a call phone number and minutes used
 
-    // getRemainingMinutes() {
-    //     super.getRemainingMinutes
-    // }
+// See the call history as a string.
+getHistory() {
+    return this.history;
+};
+
+
+}
+ 
 
     export default CellPhone;
--- main.js
+++ main.js
@@ -4,24 +4,38 @@ import CallingCard from "./CallingCard.js";
 import CellPhone from "./CellPhone.js";
 
 
-let card = new CallingCard(10);
-card.addDollars(1); // add 100 cents @ 10 cents/minute = 10 minutes added
+let card = new CallingCard(20);
+card.addDollars(1); // add 100 cents @ 20 cents/minute = 5 minutes added
 
 let phone = new CellPhone(card);
-phone.isTalking();  // => returns false
-console.log(phone.isTalking());
+phone.call("555-1111");
+phone.tick();       // 1 minute elapsed
+phone.tick();       // 2 minutes elapsed
+phone.endCall();
 
-phone.call("555-1212");
-phone.isTalking();  // => returns true
-console.log(phone.isTalking());
+phone.call("555-3333");
+phone.tick();       // 3 minutes elapsed
+phone.tick();       // 4 minutes elapsed
+phone.tick();       // this is the end of the 5th minute, so the call is ended
 
-phone.tick();       // simulate a minute going by
-phone.tick();       // simulate another minute going by
+phone.getHistory(); // => returns "555-1111 (2 minutes), 555-3333 (cut of at 3 minutes)"
 
-//phone.endCall();
-phone.isTalking();  // => returns false (because the call is over)
-//phone.getHistory(); // => returns "555-1212 (2 minutes)"
-
-card.getRemainingMinutes() // => return 8, because the call lasted 2 minutes;
-
+card.getRemainingMinutes() // => returns 0, because all 5 minutes have been used up
 console.log(phone);
+
+let phone2 = new CellPhone(card);
+card.addDollars(1); // add 100 cents @ 20 cents/minute = 5 minutes added
+phone2.call("555-4444");
+phone2.tick();       // 1 minute elapsed
+phone2.tick();       // 2 minutes elapsed
+phone2.tick();       // 2 minutes elapsed
+phone2.tick();       // 2 minutes elapsed
+phone.endCall();
+
+phone2.call("555-5555");
+phone2.tick();       // 3 minutes elapsed
+
+phone2.getHistory(); // => returns "555-1111 (2 minutes), 555-3333 (cut of at 3 minutes)"
+
+card.getRemainingMinutes() // => returns 0, because all 5 minutes have been used up
+console.log(phone2);
